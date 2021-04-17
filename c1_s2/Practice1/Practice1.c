047f42b546bcbcff4f565f8007075954ad51bc7d
Dmitriy <troi51@yandex.ru>
2021-04-17 17:28:47 +0500

    14

--- c1_s2/Practice1/Practice1.c
+++ c1_s2/Practice1/Practice1.c
@@ -1,15 +1,14 @@
-п»ї//Р›Р°Р±РѕСЂР°С‚РѕСЂРЅР°СЏ СЂР°Р±РѕС‚Р° в„–1
+//Лабораторная работа №1
 #include <stdio.h> 
 #include <locale.h>
 
 int main(void)
 {
 	setlocale(LC_ALL, "Russian");
-	system("chcp 1251");
 	int a, b;
-	printf("Р№С†Сѓ:");
+	printf("йцу:");
 	scanf_s("%i", &a);
-	printf("Р№С†Сѓ");
+	printf("йцу");
 	scanf_s("%i", &b);
 
 

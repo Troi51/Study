eecaf8373548db66e7fcbca3f58072853c00c2bd
Dmitriy <troi51@yandex.ru>
2021-04-18 19:59:45 +0500

    1.0

---
+++ c1_s2/Practice2/Practice2.c
@@ -0,0 +1,17 @@
+﻿//Практическая работа №2
+//Студент ИВТб20з-1 Ряжкин Дмитрий Андреевич
+//18.04.2021
+
+#include <stdio.h> 
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    //Сообщаем компилятору, что хотим видеть в командной строке русские буквы вместо "крокозябр" 
+    setlocale(LC_ALL, "Rus");
+   
+    system("pause");
+    return(0);
+}

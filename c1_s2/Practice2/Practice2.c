96735d6a6ad48fc2525b4906c4d7656c84f344cd
Dmitriy <troi51@yandex.ru>
2021-04-18 20:45:37 +0500

    1.2

--- c1_s2/Practice2/Practice2.c
+++ c1_s2/Practice2/Practice2.c
@@ -4,14 +4,23 @@
 
 #include <stdio.h> 
 #include <locale.h>
-#include <stdlib.h>
-#include <time.h>
+#include <math.h>
+ 
+//С помощью данной функции мы каждый раз сможем находить f(x)
+double f(double d_x, int i_k, int i_l, int i_m, int i_n)
+{
+    return(i_k * pow(d_x, 3) + i_l * pow(d_x, 2) + i_m * d_x + i_n);
+}
+
 
 int main(void)
 {
     //Сообщаем компилятору, что хотим видеть в командной строке русские буквы вместо "крокозябр" 
     setlocale(LC_ALL, "Rus");
    
+
+
+
     system("pause");
     return(0);
 }

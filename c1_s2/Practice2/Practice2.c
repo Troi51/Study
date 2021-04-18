0c901b142dafb140fb21b17b5e3b8c0f70df959b
Dmitriy <troi51@yandex.ru>
2021-04-18 22:32:32 +0500

    working

--- c1_s2/Practice2/Practice2.c
+++ c1_s2/Practice2/Practice2.c
@@ -10,7 +10,7 @@ int i_k, i_l, i_m, i_n;
 double d_a, d_b, d_sigma;
 
 //С помощью данной функции мы каждый раз сможем находить f(x)
-double f(double d_x, int i_k, int i_l, int i_m, int i_n)
+double f(double d_x)
 {
     double r;
     r = (i_k * pow(d_x, 3) + i_l * pow(d_x, 2) + i_m * d_x + i_n);
@@ -20,20 +20,18 @@ double f(double d_x, int i_k, int i_l, int i_m, int i_n)
 
 double find_k()
 {
-    double d_xm, d_fxm;
-    
-    do {
+    double d_xm;
+    while (d_sigma < d_b - d_a) {
         d_xm = (d_a + d_b) / 2;
-        d_fxm = f(d_xm, i_k, i_l, i_m, i_n);
-        if (d_fxm <= 0)
-        {
-            d_b = d_xm;
-        }
-        else
+        if (f(d_b) * f(d_xm) < 0)
         {
             d_a = d_xm;
         }
-    } while (d_b - d_a > d_sigma);
+        else
+        {
+            d_b = d_xm;
+        }
+    }
     return(d_xm);
 }
 
@@ -46,11 +44,11 @@ int main(void)
     scanf_s("%lf", &d_a);
     printf("Введите значение правого края интервала:");
     scanf_s("%lf", &d_b);
-    d_sigma = 0.01;
+    d_sigma = 0.1;
     i_k = 1;
     i_l = 1;
     i_m = 1;
-    i_n = 1;
+    i_n = 3;
     printf("Итог - %f", find_k());
 
     system("pause");

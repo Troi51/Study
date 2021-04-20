b06b2e3397721e2a68ae8536fba11d97535f943f
Dmitriy <troi51@yandex.ru>
2021-04-20 10:50:06 +0500

    check V2

--- c1_s2/Lab3/Exercise1.c
+++ c1_s2/Lab3/Exercise1.c
@@ -7,7 +7,21 @@
 #include <math.h>
 
 int i_a, i_b, i_c;
-char c_check[6]="";
+char c_check[6];
+
+int check_value(int i_x, char c_x, int i_a, int i_b)
+{
+    if (i_x == 0)
+    {
+        c_check[i_a] = c_x;
+        c_check[i_b] = ' ';
+    }
+    else
+    {
+        c_check[i_a] = ' ';
+        c_check[i_b] = ' ';
+    }
+}
 
 
 int main(void)
@@ -17,28 +31,44 @@ int main(void)
     
     printf("Введите значение коэффицента а:");
     scanf_s("%ld", &i_a);
-    if (i_a < -1)
+    if (i_a == 0)
     {
         c_check[0] = 'a';
         c_check[1] = ' ';
     }
+    else
+    {
+        c_check[0] = ' ';
+        c_check[1] = ' ';
+    }
     printf("Введите значение коэффицента b:");
     scanf_s("%ld", &i_b);
-    if (i_b < -1)
+    if (i_b == 0)
     {
         c_check[2] = 'b';
         c_check[3] = ' ';
     }
+    else
+    {
+        c_check[2] = ' ';
+        c_check[3] = ' ';
+    }
     printf("Введите значение коэффицента c:");
     scanf_s("%ld", &i_c);
-    if (i_c < -1)
+    if (i_c == 0)
     {
         c_check[4] = 'c';
         c_check[5] = ' ';
     }
-    if ((i_a < -1) | (i_b < -1) | (i_c < -1))
+    else
     {
-        printf("Данные значения = 0: %s", c_check);
+        c_check[4] = ' ';
+        c_check[5] = ' ';
+    }
+    if ((i_a == 0) | (i_b == 0) | (i_c == 0))
+    {
+        system("cls");
+        printf("Данные значения равны нулю: %s\nПроверьте правильность данных и повторите ввод.", c_check);
     }
     
 
